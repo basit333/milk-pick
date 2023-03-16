460cdd6ba43e794ba754fc4deaaa6a68f83969cf
basit333 <shahbasit741@gmail.com>
2023-03-16 22:07:22 +0500

    latest code

--- js/app.js
+++ js/app.js
@@ -1,24 +1,22 @@
-const sidebarToggle = document.querySelector("#nav-toggle");
+const sidebarToggle = document.querySelector(".milk-pick-product-toggle");
 const sidebar = document.querySelector(".milk-pick-product-left");
 const hamburger = document.querySelector(".hamburger");
-const hamburgerOpenImg = document.querySelector(".hamburger-open-img");
-console.log(hamburgerOpenImg);
-const hamburgerCloseImg = document.querySelector(".hamburger-close-img");
-console.log(hamburgerCloseImg);
+const hamburgerIcon = document.querySelector(".hamburger-icon");
 const navItems = document.querySelector(".nav-items");
 const navItemsClose = document.querySelector(".nav-item--close");
+const filterCloseBtn = document.querySelector(".filter-close-btn");
+console.log(filterCloseBtn);
 
 sidebarToggle.addEventListener("click", () => sidebar.classList.toggle("active"));
+filterCloseBtn.addEventListener("click", () => sidebar.classList.remove("active"));
 hamburger.addEventListener("click", () => navItems.classList.toggle("active"));
-hamburgerOpenImg.addEventListener("click", () => navItems.classList.add("active"));
-hamburgerCloseImg.addEventListener("click", () => navItems.classList.remove("active"));
 navItemsClose.addEventListener("click", () => navItems.classList.toggle("active"));
 
 document.addEventListener("click", (event) => {
   if (!sidebar.contains(event.target) && event.target !== sidebarToggle) {
     sidebar.classList.remove("active");
   }
-  if (!navItems.contains(event.target) && event.target !== hamburger) {
+  if (!navItems.contains(event.target) && event.target !== hamburgerIcon) {
     navItems.classList.remove("active");
   }
 });
